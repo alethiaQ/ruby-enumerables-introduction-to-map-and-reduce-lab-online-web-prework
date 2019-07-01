59933ed5d96c9ae2746787b05025ac6b5ab21aff
Alethia Quintero <alethiaq22@gmail.com>
2019-07-01 00:42:04 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,4 +2,8 @@
 def map_to_negativize(source_array) 
   i = 0
   while i < source_array.length do
-    
+    source_array[i] = (source_array[i] * -1) 
+    return source_array[i]
+  end
+  i += 1 
+end 
