ad392b26c54592d9f27decdb2b9505e7132f70bb
Alethia Quintero <alethiaq22@gmail.com>
2019-07-01 01:28:04 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -48,6 +48,11 @@ def reduce_to_total(source_array, starting_point=0)
   return total
 end 
 
-
+def reduce_to_all_true(source_array)
+  i = 0 
+  while i < source_array.length do 
+    source_array[i] = true 
+    i += 1
+  end
 
 
